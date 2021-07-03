7ff5c20118d4860ee3abcac5552b7313753b975b
himanshu <himanshu.gusain1989@gmail.com>
2021-07-03 20:54:42 +0530

    checking system command vulnerability

--- sampleCode.c
+++ sampleCode.c
@@ -9,14 +9,19 @@ int main()
 	FILE *fp = fopen(TESTFILE,"r");
 	if ( fp != NULL )
 	{
+		printf("able to open file\n");
+
 		system("ps");
 		system("ls");
 		system("ls -al");
+		fclose(fp);
 		ret=0;
 	}else
 	{
+		printf("Unable to open file\n");
 		ret=1;
 	}
 
+	system("ps -aux");
 	return ret;
 }
