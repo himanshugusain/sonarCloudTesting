8c16b2466b5df25b9475c59ce597426c7cb15fd2
himanshu <himanshu.gusain1989@gmail.com>
2021-07-03 20:47:27 +0530

    checking system command vulnerability

--- sampleCode.c
+++ sampleCode.c
@@ -2,9 +2,21 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
+#define TESTFILE	"/tmp/testfile"
 int main()
 {
 	int ret;
-	system("ps");
-	return 0;
+	FILE *fp = fopen(TESTFILE,"r");
+	if ( fp != NULL )
+	{
+		system("ps");
+		system("ls");
+		system("ls -al");
+		ret=0;
+	}else
+	{
+		ret=1;
+	}
+
+	return ret;
 }
