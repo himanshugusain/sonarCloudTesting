06f4447dba545bfeb9b8861319b8ee0fb657a666
himanshu <himanshu.gusain1989@gmail.com>
2021-07-03 16:17:41 +0530

    just checking system command

--- sampleCode.c
+++ sampleCode.c
@@ -5,15 +5,6 @@
 int main()
 {
 	int ret;
-	ret = WEXITSTATUS(system("ps"));
-	if (ret != 0)
-	{
-		printf("failure in execution of ps command\n");
-		return 1;
-	}else
-	{
-	
-		printf("success in execution of ps command\n");
-	}
+	system("ps");
 	return 0;
 }
